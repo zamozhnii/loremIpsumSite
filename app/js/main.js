8d2bf5ed4f8045e12e7654d354fe3c8fd5a24d4b
Igor Zamozhnii <zamozhnii@gmail.com>
2018-02-06 00:11:14 +0200

    parallax

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,1 @@
-function func() {
-    let a = 0;
-    let b = [1, 2, 3];
-    b.forEach(el => {
-        console.log(el);
-    });
-    console.log("acd");
-}
-let a = 28;
-console.log(a + 28);
+$('.maincontent__parallax').parallax({ imageSrc: '../img/image.jpg' });
